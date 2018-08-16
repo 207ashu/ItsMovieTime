1ab6638d5718d97c0c13ba8a7bf1d7bfe41529a9
207ashu <207ashu@gmail.com>
2018-08-16 15:08:32 +0530

    pojo class

---
+++ app/src/main/java/com/example/ashu/itsmovietime/MovieItem.java
@@ -0,0 +1,37 @@
+package com.example.ashu.itsmovietime;
+
+public class MovieItem {
+    private String title;
+    private String posterPath;
+    private String movieId;
+
+    public MovieItem(String title, String posterPath,String movieId) {
+        this.title = title;
+        this.posterPath = posterPath;
+        this.movieId=movieId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+}
